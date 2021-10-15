a986839fdbdc22a96f7e360e23f1d281026927a8
YanSakhnevich <yansakhnevich@gmail.com>
2021-10-16 10:51:00 +1100

    Project "Navigation" created

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Yan Sakhnevich on 16.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
