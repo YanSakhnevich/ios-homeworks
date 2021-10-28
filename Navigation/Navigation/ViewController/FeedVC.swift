f8cf5ae176d798f8b0413f31c4dcdc1051894130
YanSakhnevich <yansakhnevich@gmail.com>
2021-10-29 00:05:55 +1100

    Task2.2_Completed

--- Navigation/Navigation/ViewController/FeedVC.swift
+++ Navigation/Navigation/ViewController/FeedVC.swift
@@ -6,7 +6,7 @@ class FeedVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
+        view.backgroundColor = .systemPink
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = .systemBackground
         appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
@@ -15,14 +15,48 @@ class FeedVC: UIViewController {
         navigationController?.navigationBar.compactAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         
+        configureStack()
+        view.addSubview(stack)
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        stack.spacing = 10
+        stack.toAutoLayout()
+        setupLayout()
     }
    
+    var stack = UIStackView()
     
-  
+    func configureStack() {
+        let _ = ["Button_1", "Button_2"].map { [weak self] txt in
+            let button = MyButton()
+            button.blueButton(title: txt)
+            button.toAutoLayout()
+            button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+            self?.stack.addArrangedSubview(button)
+        }
+    }
     
-    struct Post {
-        let title: String
+    @objc func didTapButton() {
+        let post = PostVC(titlePost: titlePostVC.title)
+        navigationController?.pushViewController(post, animated: true)
+    }
+    
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            stack.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            stack.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
 }
+
+
+struct Post {
+    let title: String
+}
+
+
+
+
 
 
